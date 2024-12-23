f6f66713625fcbbf4acba63db7dbc0660795622a
Azaria <askelman5@gmail.com>
2024-12-22 20:13:09 -0500

    Defined layout, improved CSS

--- LuminaryAI-Vite/src/App.jsx
+++ LuminaryAI-Vite/src/App.jsx
@@ -1,12 +1,16 @@
 import Whiteboard from './components/whiteboard';
 import Header from './components/header';
+import Footer from './components/footer';
 import './App.css';
 
 export default function App() {
     return (
         <div>
             <Header />
+
             <Whiteboard />
+
+            <Footer />
         </div>
     );
 }
--- LuminaryAI-Vite/src/components/whiteboard.jsx
+++ LuminaryAI-Vite/src/components/whiteboard.jsx
@@ -1,11 +1,24 @@
 import { Tldraw } from 'tldraw'
 import 'tldraw/tldraw.css'
 
-
 export default function App() {
-	return (
-		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw inferDarkMode={true} />
-		</div>
-	)
+
+    const whiteboardWidth = 55;
+    const marginLeft = 100 - whiteboardWidth;
+
+    const whiteboardHeight = 80;
+    const marginTop = 95 - whiteboardHeight;
+    
+    return (
+        <div style={{ 
+            position: 'fixed', 
+            inset: 0, 
+            width: `${whiteboardWidth}vw`, 
+            height: `${whiteboardHeight}vh`,
+            marginLeft: `${marginLeft}vw`, 
+            marginTop: `${marginTop}vh`
+        }}>
+            <Tldraw inferDarkMode={true} />
+        </div>
+    )
 }
