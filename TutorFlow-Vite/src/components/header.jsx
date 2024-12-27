f5ad65286c76d5f5758c1c2f7a277ea116f96eaa
Azaria <askelman5@gmail.com>
2024-12-27 15:18:23 -0500

    Added icons

--- TutorFlow-Vite/src/components/header.jsx
+++ TutorFlow-Vite/src/components/header.jsx
@@ -1,4 +1,7 @@
-// import icon from './assets/icons/downloadIcon.png';
+import downloadIcon from '../assets/icons/download-icon.png';
+import settingsIcon from '../assets/icons/settings-icon.png';
+import helpIcon from '../assets/icons/help-icon.png';
+
 
 export default function Header() {
     return (
@@ -8,7 +11,7 @@ export default function Header() {
                 justifyContent: 'space-between', 
                 alignItems: 'center', 
                 marginLeft: '3vw',
-                marginRight: '10vw',
+                marginRight: '5vw',
                 marginTop: '3vh',
                 fontSize: '22px'
             }}>
@@ -18,7 +21,29 @@ export default function Header() {
                 <span style={{
                     fontSize: '22px', // Smaller font size for the latter half
                 }}>
-                    Interactive Learning Made Simple.
+                    Interactive Learning Made Simple. 
+
+                    <img 
+                                src={downloadIcon} 
+                                alt="Download" 
+                                style={{ width: '2vw', height: '2vw', marginLeft: '1vw', verticalAlign: 'text-top' }}
+
+                    />
+
+                    <img 
+                                src={helpIcon} 
+                                alt="Help" 
+                                style={{ width: '2vw', height: '2vw', marginLeft: '0.5vw', verticalAlign: 'text-top' }}
+
+                    />
+
+                    <img 
+                                src={settingsIcon} 
+                                alt="Settings" 
+                                style={{ width: '2vw', height: '2vw', marginLeft: '0.5vw', verticalAlign: 'text-top' }}
+
+                    />
+
                 </span>
             </div>
         </header>
