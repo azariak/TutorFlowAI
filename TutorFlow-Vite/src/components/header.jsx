fd540d3b3f1ac8137396173769f0f6564a7254db
Azaria <askelman5@gmail.com>
2024-12-28 21:05:23 -0500

    Added link to header

--- TutorFlow-Vite/src/components/header.jsx
+++ TutorFlow-Vite/src/components/header.jsx
@@ -1,51 +1,67 @@
-import downloadIcon from '../assets/icons/download-icon.png';
-import settingsIcon from '../assets/icons/settings-icon.png';
-import helpIcon from '../assets/icons/help-icon.png';
-
+import downloadIcon from "../assets/icons/download-icon.png";
+import settingsIcon from "../assets/icons/settings-icon.png";
+import helpIcon from "../assets/icons/help-icon.png";
 
 export default function Header() {
-    return (
-        <header>
-            <div style={{
-                display: 'flex', 
-                justifyContent: 'space-between', 
-                alignItems: 'center', 
-                marginLeft: '3vw',
-                marginRight: '5vw',
-                marginTop: '3vh',
-                fontSize: '22px'
-            }}>
-                <h1 style={{ margin: 0 }}>
-                    TutorFlowAI
-                </h1>
-                <span style={{
-                    fontSize: '22px', // Smaller font size for the latter half
-                }}>
-                    Interactive Learning Made Simple. 
-
-                    <img 
-                                src={downloadIcon} 
-                                alt="Download" 
-                                style={{ width: '2vw', height: '2vw', marginLeft: '1vw', verticalAlign: 'text-top' }}
-
-                    />
-
-                    <img 
-                                src={helpIcon} 
-                                alt="Help" 
-                                style={{ width: '2vw', height: '2vw', marginLeft: '0.5vw', verticalAlign: 'text-top' }}
-
-                    />
-
-                    <img 
-                                src={settingsIcon} 
-                                alt="Settings" 
-                                style={{ width: '2vw', height: '2vw', marginLeft: '0.5vw', verticalAlign: 'text-top' }}
-
-                    />
-
-                </span>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginLeft: "3vw",
+          marginRight: "5vw",
+          marginTop: "3vh",
+          fontSize: "22px",
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <a
+            href="https://tutorflowai.com"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            TutorFlowAI{" "}
+          </a>
+        </h1>
+        <span
+          style={{
+            fontSize: "22px", // Smaller font size for the latter half
+          }}
+        >
+          Interactive Learning Made Simple.
+          <img
+            src={downloadIcon}
+            alt="Download"
+            style={{
+              width: "2vw",
+              height: "2vw",
+              marginLeft: "1vw",
+              verticalAlign: "text-top",
+            }}
+          />
+          <img
+            src={helpIcon}
+            alt="Help"
+            style={{
+              width: "2vw",
+              height: "2vw",
+              marginLeft: "0.5vw",
+              verticalAlign: "text-top",
+            }}
+          />
+          <img
+            src={settingsIcon}
+            alt="Settings"
+            style={{
+              width: "2vw",
+              height: "2vw",
+              marginLeft: "0.5vw",
+              verticalAlign: "text-top",
+            }}
+          />
+        </span>
+      </div>
+    </header>
+  );
 }
