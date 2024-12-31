6b42b5a5d42999a2c14379c94c016fca4715693a
Azaria <askelman5@gmail.com>
2024-12-30 23:16:35 -0500

    acsa

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -103,7 +103,7 @@ export default function App() {
           image: undefined
         }));
     
-        const response = await fetch('/functions/generate', {
+        const response = await fetch('/api/generate', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
