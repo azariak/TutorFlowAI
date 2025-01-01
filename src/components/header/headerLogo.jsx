f080a64ca19057e6806886dd9e4383798a91ecce
Azaria <askelman5@gmail.com>
2024-12-31 22:40:06 -0500

    Updated file structure

--- src/components/header/headerLogo.jsx
+++ src/components/header/headerLogo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/brand/logo.png';
 import { useHeaderStyles } from './styles';
 
 export function HeaderLogo() {
